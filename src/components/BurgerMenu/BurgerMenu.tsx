1fb8011bb230a136e73f466775d23914adc6c865
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-08 16:37:08 +0300

    1 Has been creaeted files structure the BurgerMenu component

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { BurgerMenuWrapper } from './BurgerMenu.styled';
+
+interface BurgerMenuProps {}
+
+const BurgerMenu: FC<BurgerMenuProps> = () => (
+ <BurgerMenuWrapper>
+    BurgerMenu Component
+ </BurgerMenuWrapper>
+);
+
+export default BurgerMenu;
