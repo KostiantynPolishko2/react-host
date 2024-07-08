157b057d2ec0367d97215893efa27b6341a857b6
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-08 16:55:32 +0300

    2 Has nested, styled BurgerMenu

--- src/components/BurgerMenu/BurgerMenu.styled.ts
+++ src/components/BurgerMenu/BurgerMenu.styled.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const BurgerMenuWrapper = styled.div`
+
 `;
--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,12 +1,38 @@
 import React, { FC } from 'react';
 import { BurgerMenuWrapper } from './BurgerMenu.styled';
+import './BurgerMenu.css';
+import Hamburger from 'hamburger-react';
 
 interface BurgerMenuProps {}
 
-const BurgerMenu: FC<BurgerMenuProps> = () => (
- <BurgerMenuWrapper>
-    BurgerMenu Component
- </BurgerMenuWrapper>
-);
+const hideShowBurger = (flag: boolean, links: HTMLElement) => {
+
+   flag? console.log('ShowBurger') : console.log('HideBurger');
+   console.log('links', links?.style.display);
+   // flag? links.style.display = 'block' : links.style.display = 'none';
+}
+
+const BurgerMenu: FC<BurgerMenuProps> = (): React.FunctionComponentElement<BurgerMenuProps> => {
+
+   const links = document.getElementById('myLinks') as HTMLElement;
+
+   return (
+      <BurgerMenuWrapper className='topnav'>
+         <div style={{backgroundColor: 'grey', width: 'fit-content', height: 51 + 'px', float: 'left'}}>
+            <Hamburger color='whitesmoke' background-color='grey' rounded
+            onToggle={flag => {hideShowBurger(flag, links)}}/>
+         </div>
+         
+         <div id="myLinks">
+               <a href="#news">Number</a>
+               <a href="#contact">VinCode</a>
+               <a href="#about">MarkModel</a>
+               <a href="#about">Region</a>
+               <a href="#about">Price</a>
+         </div>
+         <a href="#home" className="active">Compare</a>
+      </BurgerMenuWrapper>
+     );
+}
 
 export default BurgerMenu;
